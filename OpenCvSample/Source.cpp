e5c68735e8ad7887f4c685c9093bb144e9bf1582
Martin Erdély <martyn.sniper@gmail.com>
2018-10-10 09:48:43 +0200

    FTTW install

--- OpenCvSample/Source.cpp
+++ OpenCvSample/Source.cpp
@@ -4,12 +4,31 @@
 #include "opencv2/videoio.hpp"
 #include "opencv2/highgui.hpp"
 
+
+#include <fftw3.h>
+
 using namespace cv;
 int main(int argc, const char** argv)
 {
 	Mat image;
 	image = imread("down.png", CV_LOAD_IMAGE_COLOR);
 	imshow("Test", image);
+
+	fftw_complex *in, *out;
+	fftw_plan p;
+
+	int N = 32;
+
+	in = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * N);
+	out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * N);
+	p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+
+	fftw_execute(p); /* repeat as needed */
+
+	fftw_destroy_plan(p);
+	fftw_free(in); fftw_free(out);
+
 	waitKey(0);
+
 	return 0;
 }
