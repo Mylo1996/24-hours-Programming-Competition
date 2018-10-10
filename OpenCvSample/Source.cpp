5ec9e933df9c4e885c8a04dca360c8b6b14f80a5
Martin Erdély <martyn.sniper@gmail.com>
2018-10-10 16:32:07 +0200

    simple record

--- OpenCvSample/Source.cpp
+++ OpenCvSample/Source.cpp
@@ -8,100 +8,9 @@
 #include <iostream>
 #include <fstream>
 #include <bitset>
+#include <time.h> 
+#include <Windows.h>
 
-
-// Useful to hold onto the memory when converting it into a Chunk.
-struct Samples {
-	Samples(sf::Int16 const* ss, std::size_t count) {
-		samples.reserve(count);
-		std::copy_n(ss, count, std::back_inserter(samples));
-	}
-
-	Samples() {}
-
-	std::vector<sf::Int16> samples;
-};
-
-
-class PlaybackRecorder : private sf::SoundRecorder, private sf::SoundStream {
-public: /** API **/
-
-	// Initialise capturing input & setup output
-	void start() {
-		sf::SoundRecorder::start();
-
-		sf::SoundStream::initialize(sf::SoundRecorder::getChannelCount(), sf::SoundRecorder::getSampleRate());
-		sf::SoundStream::play();
-	}
-
-	// Stop both recording & playback
-	void stop() {
-		sf::SoundRecorder::stop();
-		sf::SoundStream::stop();
-	}
-
-	bool isRunning() { return isRecording; }
-
-
-	~PlaybackRecorder() {
-		stop();
-	}
-
-
-protected: /** OVERRIDING SoundRecorder **/
-
-	bool onProcessSamples(sf::Int16 const* samples, std::size_t sampleCount) override {
-		{
-			std::lock_guard<std::mutex> lock(mutex);
-			data.emplace(samples, sampleCount);
-		}
-		cv.notify_one();
-		return true; // continue capture
-	}
-
-	bool onStart() override {
-		isRecording = true;
-		return true;
-	}
-
-	void onStop() override {
-		isRecording = false;
-		cv.notify_one();
-	}
-
-
-protected: /** OVERRIDING SoundStream **/
-
-	bool onGetData(Chunk& chunk) override {
-		// Wait until either:
-		//  a) the recording was stopped
-		//  b) new data is available
-		std::unique_lock<std::mutex> lock(mutex);
-		cv.wait(lock, [this] { return !isRecording || !data.empty(); });
-
-		// Lock was acquired, examine which case we're into:
-		if (!isRecording) return false; // stop playing.
-		else {
-			assert(!data.empty());
-
-			playingSamples.samples = std::move(data.front().samples);
-			data.pop();
-			chunk.sampleCount = playingSamples.samples.size();
-			chunk.samples = playingSamples.samples.data();
-			return true;
-		}
-	}
-
-	void onSeek(sf::Time) override { /* Not supported, silently does nothing. */ }
-
-private:
-	std::atomic<bool> isRecording{ false };
-	std::mutex mutex; // protects `data`
-	std::condition_variable cv; // notify consumer thread of new samples
-	std::queue<Samples> data; // samples come in from the recorder, and popped by the output stream
-	Samples playingSamples; // used by the output stream.
-};
-using namespace cv;
 using namespace std;
 
 vector<char> FtoBA(string filename) {
@@ -151,47 +60,29 @@ vector<int> BAtoIA(vector<char> vc) {
 	return vi;
 }
 
-int main(int argc, const char** argv)
-{
-
-	//Tesztfutás függvényekre
-
-	//BAtoF("kki.txt", IAtoBA(BAtoIA(FtoBA("be.txt"))));
-	
-
-
-	Mat image;
-	image = imread("down.png", CV_LOAD_IMAGE_COLOR);
-	imshow("Test", image);
 
 int main(int, char const**)
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML PlayBack");
-
-	if (!sf::SoundRecorder::isAvailable()) {
-		return EXIT_FAILURE;
-	}
-
-	PlaybackRecorder input;
-	input.start();
-
-	while (window.isOpen())
+	if (!sf::SoundBufferRecorder::isAvailable())
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed) {
-				window.close();
-			}
-
-			if (event.type == sf::Event::KeyPressed) {
-				if (input.isRunning()) input.stop();
-				else input.start();
-			}
-		}
-
-		window.display();
 	}
+	// create the recorder
+	sf::SoundBufferRecorder recorder;
+	// start the capture
+	recorder.start();
+	Sleep(500);
+	cout << "Speak now" << endl;
+	Sleep(2000);
+	// stop the capture
+	recorder.stop();
+	cout << "Stop" << endl;
+	// retrieve the buffer that contains the captured audio data
+	const sf::SoundBuffer& buffer = recorder.getBuffer();
+	sf::Sound sound(buffer);
+	Sleep(100);
+	sound.play();
+
+	Sleep(3000);
 
 	return EXIT_SUCCESS;
 }
