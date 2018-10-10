46cb9890a4f8a763a2e32b57f02407ee80c52917
Martin Erdély <martyn.sniper@gmail.com>
2018-10-10 16:11:11 +0200

    try

--- OpenCvSample/Source.cpp
+++ OpenCvSample/Source.cpp
@@ -1,34 +1,132 @@
-#include <opencv2/core/utility.hpp>
-#include "opencv2/video/tracking.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/videoio.hpp"
-#include "opencv2/highgui.hpp"
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+#include <atomic>
+#include <cassert>
+#include <mutex>
+#include <queue>
 
 
-#include <fftw3.h>
+// Useful to hold onto the memory when converting it into a Chunk.
+struct Samples {
+	Samples(sf::Int16 const* ss, std::size_t count) {
+		samples.reserve(count);
+		std::copy_n(ss, count, std::back_inserter(samples));
+	}
 
-using namespace cv;
-int main(int argc, const char** argv)
+	Samples() {}
+
+	std::vector<sf::Int16> samples;
+};
+
+
+class PlaybackRecorder : private sf::SoundRecorder, private sf::SoundStream {
+public: /** API **/
+
+	// Initialise capturing input & setup output
+	void start() {
+		sf::SoundRecorder::start();
+
+		sf::SoundStream::initialize(sf::SoundRecorder::getChannelCount(), sf::SoundRecorder::getSampleRate());
+		sf::SoundStream::play();
+	}
+
+	// Stop both recording & playback
+	void stop() {
+		sf::SoundRecorder::stop();
+		sf::SoundStream::stop();
+	}
+
+	bool isRunning() { return isRecording; }
+
+
+	~PlaybackRecorder() {
+		stop();
+	}
+
+
+protected: /** OVERRIDING SoundRecorder **/
+
+	bool onProcessSamples(sf::Int16 const* samples, std::size_t sampleCount) override {
+		{
+			std::lock_guard<std::mutex> lock(mutex);
+			data.emplace(samples, sampleCount);
+		}
+		cv.notify_one();
+		return true; // continue capture
+	}
+
+	bool onStart() override {
+		isRecording = true;
+		return true;
+	}
+
+	void onStop() override {
+		isRecording = false;
+		cv.notify_one();
+	}
+
+
+protected: /** OVERRIDING SoundStream **/
+
+	bool onGetData(Chunk& chunk) override {
+		// Wait until either:
+		//  a) the recording was stopped
+		//  b) new data is available
+		std::unique_lock<std::mutex> lock(mutex);
+		cv.wait(lock, [this] { return !isRecording || !data.empty(); });
+
+		// Lock was acquired, examine which case we're into:
+		if (!isRecording) return false; // stop playing.
+		else {
+			assert(!data.empty());
+
+			playingSamples.samples = std::move(data.front().samples);
+			data.pop();
+			chunk.sampleCount = playingSamples.samples.size();
+			chunk.samples = playingSamples.samples.data();
+			return true;
+		}
+	}
+
+	void onSeek(sf::Time) override { /* Not supported, silently does nothing. */ }
+
+private:
+	std::atomic<bool> isRecording{ false };
+	std::mutex mutex; // protects `data`
+	std::condition_variable cv; // notify consumer thread of new samples
+	std::queue<Samples> data; // samples come in from the recorder, and popped by the output stream
+	Samples playingSamples; // used by the output stream.
+};
+
+int main(int, char const**)
 {
-	Mat image;
-	image = imread("down.png", CV_LOAD_IMAGE_COLOR);
-	imshow("Test", image);
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML PlayBack");
 
-	fftw_complex *in, *out;
-	fftw_plan p;
+	if (!sf::SoundRecorder::isAvailable()) {
+		return EXIT_FAILURE;
+	}
 
-	int N = 32;
+	PlaybackRecorder input;
+	input.start();
 
-	in = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * N);
-	out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * N);
-	p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
 
-	fftw_execute(p); /* repeat as needed */
+			if (event.type == sf::Event::KeyPressed) {
+				if (input.isRunning()) input.stop();
+				else input.start();
+			}
+		}
 
-	fftw_destroy_plan(p);
-	fftw_free(in); fftw_free(out);
+		window.display();
+	}
 
-	waitKey(0);
-
-	return 0;
+	return EXIT_SUCCESS;
 }
