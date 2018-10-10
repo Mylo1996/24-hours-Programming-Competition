97acb1d3afc05c6aaf503894dc6727ef01371f41
Martin Erdély <martyn.sniper@gmail.com>
2018-10-10 07:26:31 +0200

    test2

--- OpenCvSample/Source.cpp
+++ OpenCvSample/Source.cpp
@@ -8,7 +8,6 @@ using namespace cv;
 int main(int argc, const char** argv)
 {
 	Mat image;
-	// Allahusdfkbar
 	image = imread("down.png", CV_LOAD_IMAGE_COLOR);
 	imshow("Test", image);
 	waitKey(0);
